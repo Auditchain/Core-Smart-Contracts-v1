23fc2b116c453964c23a75584cb2f628f47b5781
Bogdan Feidur <win2win@protonmail.com>
2021-03-09 15:58:17 -0600

    Update license statement

--- contracts/Governance/Timelock.sol
+++ contracts/Governance/Timelock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-pragma solidity >=0.6.0 <0.8.0;Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.8.0;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
