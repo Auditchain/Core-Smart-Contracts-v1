e293f1b5933ed534dd58884fe3d0dcfa0630d0b3
Bogdan Fiedur <win2win@protonmail.com>
2021-09-18 11:15:01 -0500

    modify returnDelegator link

--- contracts/Auditchain/INodeOperations.sol
+++ contracts/Auditchain/INodeOperations.sol
@@ -3,11 +3,12 @@ pragma solidity =0.8.0;
     
 interface INodeOperations {
 
-    function delegatorLink(address delegatee) external view returns (address);
     function increasePOWRewards(address validator, uint256 amount) external ;
     function increaseStakeRewards(address validator) external;
     function increaseDelegatedStakeRewards(address validator) external;
     function returnNodeOperators() external view returns (address[] memory) ;
     function POWFee() external view returns (uint256);
+    function returnDelegatorLink(address operator) external view returns (address);
+    
 
 }
