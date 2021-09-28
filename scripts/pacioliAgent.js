b35e3430235272b60a01211f3203f8d66b1707fa
Bogdan Fiedur <win2win@protonmail.com>
2021-09-28 15:29:45 -0500

    modify to allow on passing private key as argument

--- scripts/pacioliAgent.js
+++ scripts/pacioliAgent.js
@@ -10,14 +10,14 @@ let ipfsAPI = require("ipfs-api");
 let BN = require("big-number");
 
 let HDWalletProvider = require('@truffle/hdwallet-provider');
-require('dotenv').config({ path: '../.env' }); // update process.env
+require('dotenv').config({ path: './.env' }); // update process.env
 
 const NON_COHORT = require('../build/contracts/ValidationsNoCohort.json');
 const MEMBER_HELPERS = require('../build/contracts/MemberHelpers.json');
 const NODE_OPERATIONS = require('../build/contracts/NodeOperations.json')
 
 //TODO: this module is still copied from https://github.com/Auditchain/Reporting-Validation-Engine/tree/main/clientExamples/pacioliClient:
-const pacioli = require('./pacioliClient'); 
+const pacioli = require('./pacioliClient');
 
 // import ethereum connection strings. 
 const ropsten_infura_server = process.env.ROPSTEN_INFURA_SERVER;
@@ -38,14 +38,6 @@ const nodeOperationsAddress = process.env.NODE_OPERATIONS_ADDRESS;
 const provider = new Web3.providers.WebsocketProvider(process.env.WEBSOCKET_PROVIDER); // e.g. 'ws://localhost:8545'
 const web3 = new Web3(provider);
 
-const providerForUpdate = new HDWalletProvider(mnemonic, local_host); // change to main_infura_server or another testnet. 
-const web3Update = new Web3(providerForUpdate);
-let nonCohortValidate = new web3Update.eth.Contract(NON_COHORT["abi"], nonCohortAddress);
-let memberHelpers = new web3Update.eth.Contract(MEMBER_HELPERS["abi"], memberHelpersAddress);
-let nodeOperations = new web3Update.eth.Contract(NODE_OPERATIONS["abi"], nodeOperationsAddress);
-
-
-
 let nonCohort = new web3.eth.Contract(NON_COHORT["abi"], nonCohortAddress);
 let ipfsBase = 'https://ipfs.io/ipfs/';
 
@@ -53,7 +45,17 @@ let ipfs = ipfsAPI('ipfs.infura.io', 5001, {
     protocol: 'https'
 }) // Connect to IPFS
 
+let nonCohortValidate;
+let nodeOperations;
+let providerForUpdate;
 
+async function setUpContracts(account) {
+
+    providerForUpdate = new HDWalletProvider(account, local_host); // change to main_infura_server or another testnet. 
+    const web3Update = new Web3(providerForUpdate);
+    nonCohortValidate = new web3Update.eth.Contract(NON_COHORT["abi"], nonCohortAddress);
+    nodeOperations = new web3Update.eth.Contract(NODE_OPERATIONS["abi"], nodeOperationsAddress);
+}
 
 
 /**
@@ -68,8 +70,8 @@ async function verifyPacioli(metadatatUrl, trxHash) {
     const reportUrl = JSON.parse(result)["reportUrl"];
 
     console.log("[1 " + trxHash + "]" + "  Querying Pacioli " + reportUrl);
+    const reportContent = await pacioli.callRemote(reportUrl, trxHash, true);
 
-    const reportContent = await pacioli.callRemote(reportUrl,trxHash,true);
     //const reportContent = await pacioli.callLocal(reportUrl,trxHash,true); 
 
     const jsonStringFromObject = JSON.stringify(reportContent);
@@ -84,33 +86,43 @@ async function verifyPacioli(metadatatUrl, trxHash) {
             content: bufRule
         }];
     const resultPacioli = await ipfs.files.add(reportFile, { wrapWithDirectory: true }); // incompatible with callLocal!!!
-        /*BUG: somehow callLocal is causing the following to be thrown in the line ABOVE:
-        [2 0x4baa71c73c76876d8a6441b082aede53c9ed7259d7a32d1895cac17313144742]  Saving Pacioli Results to IPFS
-        Error: CONNECTION ERROR: The connection got closed with the close code `1006` and the following reason string `Socket Error: read ECONNRESET`
-            at Object.ConnectionError (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-core-helpers/lib/errors.js:66:23)
-            at Object.ConnectionCloseError (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-core-helpers/lib/errors.js:53:25)
-            at /Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-core-requestmanager/lib/index.js:119:50
-            at Map.forEach (<anonymous>)
-            at WebsocketProvider.disconnect (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-core-requestmanager/lib/index.js:118:37)
-            at WebsocketProvider.emit (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/eventemitter3/index.js:181:35)
-            at WebsocketProvider._onClose (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-providers-ws/lib/index.js:152:10)
-            at W3CWebSocket._dispatchEvent [as dispatchEvent] (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/yaeti/lib/EventTarget.js:115:12)
-            at W3CWebSocket.onClose (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/websocket/lib/W3CWebSocket.js:228:10)
-            at WebSocketConnection.<anonymous> (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/websocket/lib/W3CWebSocket.js:201:17)
-            at WebSocketConnection.emit (events.js:314:20)
-            at WebSocketConnection.handleSocketClose (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/websocket/lib/WebSocketConnection.js:389:14)
-            at Socket.emit (events.js:314:20)
-            at TCP.<anonymous> (net.js:676:12) {
-        code: 1006,
-        reason: 'Socket Error: read ECONNRESET'
-        }
-    */
+    /*BUG: somehow callLocal is causing the following to be thrown in the line ABOVE:
+    [2 0x4baa71c73c76876d8a6441b082aede53c9ed7259d7a32d1895cac17313144742]  Saving Pacioli Results to IPFS
+    Error: CONNECTION ERROR: The connection got closed with the close code `1006` and the following reason string `Socket Error: read ECONNRESET`
+        at Object.ConnectionError (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-core-helpers/lib/errors.js:66:23)
+        at Object.ConnectionCloseError (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-core-helpers/lib/errors.js:53:25)
+        at /Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-core-requestmanager/lib/index.js:119:50
+        at Map.forEach (<anonymous>)
+        at WebsocketProvider.disconnect (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-core-requestmanager/lib/index.js:118:37)
+        at WebsocketProvider.emit (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/eventemitter3/index.js:181:35)
+        at WebsocketProvider._onClose (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/web3-providers-ws/lib/index.js:152:10)
+        at W3CWebSocket._dispatchEvent [as dispatchEvent] (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/yaeti/lib/EventTarget.js:115:12)
+        at W3CWebSocket.onClose (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/websocket/lib/W3CWebSocket.js:228:10)
+        at WebSocketConnection.<anonymous> (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/websocket/lib/W3CWebSocket.js:201:17)
+        at WebSocketConnection.emit (events.js:314:20)
+        at WebSocketConnection.handleSocketClose (/Users/mc/git/Core-Smart-Contracts-v1/node_modules/web3/node_modules/websocket/lib/WebSocketConnection.js:389:14)
+        at Socket.emit (events.js:314:20)
+        at TCP.<anonymous> (net.js:676:12) {
+    code: 1006,
+    reason: 'Socket Error: read ECONNRESET'
+    }
+*/
 
     const pacioliIPFS = resultPacioli[1].hash + '/' + resultPacioli[0].path;
 
     console.log("[3 " + trxHash + "]" + "  Pacioli report saved at: " + ipfsBase + pacioliIPFS);
 
-    return pacioliIPFS;
+    //TODO: Until reading isValid flag from Pacioli report is fixed.
+    let isValid = false;
+    // try {
+    //     isValid = JSON.parse(reportContent)["isValid"];
+    // } catch (error) {
+
+    //     console.log("isvalid", error);
+    // }
+
+
+    return [pacioliIPFS, isValid];
 }
 
 
@@ -121,10 +133,15 @@ async function verifyPacioli(metadatatUrl, trxHash) {
  */
 async function getReportResult(url) {
 
-    // console.log("url from getREportResults:", url);
-    const result = await ipfs.files.cat(url);
-    const isValid = JSON.parse(result)["isValid"];
-    return isValid;
+    try {
+        // console.log("url from getREportResults:", url);
+        const result = await ipfs.files.cat(url);
+        const isValid = JSON.parse(result)["isValid"];
+        return isValid;
+    } catch (error) {
+
+        console.log(error)
+    }
 }
 
 
@@ -133,13 +150,13 @@ async function getReportResult(url) {
  * @param url url of the report to validate
  * @param reportPacioliIPFSUrl Location of Pacioli report
  */
-async function uploadMetadataToIpfs(url, reportPacioliIPFSUrl, trxHash) {
+async function uploadMetadataToIpfs(url, reportPacioliIPFSUrl, trxHash, isValid) {
 
     const reportContent = (await axios.get(ipfsBase + url)).data;
     const reportHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(reportContent));
 
     console.log("[6 " + trxHash + "]" + "  Creating metadata file.");
-    const isValid = await getReportResult(reportPacioliIPFSUrl);
+   
     let metaDataObject = {
         reportUrl: ipfsBase + url,
         reportHash: reportHash,
@@ -174,14 +191,8 @@ async function validate(hash, initTime, choice, validator, trxHash, valUrl) {
 
 
     console.log("[4 " + trxHash + "]" + "  Waiting for validation transaction to complete... ");
-    const owner = providerForUpdate.addresses[validator];
-
+    const owner = providerForUpdate.addresses[0];
     const nonce = await web3.eth.getTransactionCount(owner);
-
-    console.log("Transaction count:", await web3.eth.getTransactionCount(owner));
-
-
-    // const nonCohortValidate = await initConnection();
 
     //validate request
     nonCohortValidate.methods
@@ -201,13 +212,6 @@ async function validate(hash, initTime, choice, validator, trxHash, valUrl) {
         });
 }
 
-async function awardRewards() {
-
-
-
-}
-
-
 
 /**
  * @dev Start listening to events
@@ -215,11 +219,13 @@ async function awardRewards() {
 
 async function startProcess() {
 
+
     let myArgs = process.argv.slice(2);
 
-    const owner = providerForUpdate.addresses[Number(myArgs[0])];
+    setUpContracts(myArgs[0]);
+    const owner = providerForUpdate.addresses[0];
 
-    // const nodeOperator = await nodeOperations.methods.isNodeOperator(owner).call();
+    const nodeOperator = await nodeOperations.methods.isNodeOperator(owner).call();
     const validationStruct = await nodeOperations.methods.nodeOpStruct(owner).call();
 
     const isNodeOperator = validationStruct.isNodeOperator;
@@ -236,14 +242,12 @@ async function startProcess() {
                 const validationStruct = await nodeOperations.methods.nodeOpStruct(owner).call();
                 depositAmountBefore = validationStruct.POWAmount;
 
-                // depositAmountBefore = await nodeOperations.methods.POWAmount(owner).call();
-                console.log('myArgs: ', myArgs);
-                // console.log("transaction hash:", event.transactionHash);
                 const trxHash = event.transactionHash;
-                const reportPacioliIPFSUrl = await verifyPacioli(event.returnValues.url, trxHash);
-                const isValid = await getReportResult(reportPacioliIPFSUrl, trxHash);
-                const metaDataLink = await uploadMetadataToIpfs(event.returnValues.url, reportPacioliIPFSUrl, trxHash);
-                await validate(event.returnValues.documentHash, event.returnValues.initTime, isValid ? 1 : 2, Number(myArgs[0]), trxHash, metaDataLink)
+                const [reportPacioliIPFSUrl, isValid] = await verifyPacioli(event.returnValues.url, trxHash);
+                // const isValid = await getReportResult(reportPacioliIPFSUrl, trxHash);
+
+                const metaDataLink = await uploadMetadataToIpfs(event.returnValues.url, reportPacioliIPFSUrl, trxHash, isValid);
+                await validate(event.returnValues.documentHash, event.returnValues.initTime, isValid ? 1 : 2, owner, trxHash, metaDataLink)
 
             })
             .on('error', console.error);
@@ -252,10 +256,7 @@ async function startProcess() {
         nonCohort.events.RequestExecuted({})
             .on('data', async function (event) {
                 const validationStruct = await nodeOperations.methods.nodeOpStruct(owner).call();
-                // console.log("validation Struct:", validationStruct);
                 let balanceAfter = validationStruct.POWAmount;
-
-                // const balanceAfter = await nodeOperations.methods.POWAmount(owner).call()
                 let earned = BN(balanceAfter.toString()).minus(BN(depositAmountBefore.toString()));
                 console.log("[8  You have earned: " + earned / Math.pow(10, 18) + " AUDT.");
             })
@@ -267,7 +268,6 @@ async function startProcess() {
 
     else
         console.log("You can't validate because you are not a node operator. Please register as node operator first and restart this process.");
-
 
 }
 
