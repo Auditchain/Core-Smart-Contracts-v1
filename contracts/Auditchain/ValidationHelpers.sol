d5bf6b65e303a388c7b6121b2be5045f87ed3656
Bogdan Fiedur <win2win@protonmail.com>
2021-10-19 15:48:06 -0500

    add new functions

--- contracts/Auditchain/ValidationHelpers.sol
+++ contracts/Auditchain/ValidationHelpers.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.0;
 
-import "./Validations.sol";
+import "./IValidations.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 
 
 contract ValidationHelpers {
 
-    Validations validations;
-    
+    IValidations validations;
+    using SafeMath for uint256;
+
+       // Validation can be approved or disapproved. Initial status is undefined.
+    enum ValidationStatus {Undefined, Yes, No}   
 
 
     constructor(address validationsAddress) {
 
-        validations = Validations(validationsAddress);
+        validations = IValidations(validationsAddress);
     }
 
 
@@ -37,7 +42,7 @@ contract ValidationHelpers {
     }
 
 
-    function selectWinner(bytes32 validationHash, address[] memory winners) internal returns (address) {
+    function selectWinner(bytes32 validationHash, address[] memory winners) internal view returns (address) {
 
         address winner = winners[0];
 
@@ -46,18 +51,101 @@ contract ValidationHelpers {
            (uint256 plusBefore, uint256 minusBefore) =  validations.returnWinnerPoints(validationHash, winners[i-1]);
 
             if (plus > minus)
-                if (
-                    plus.sub(validation.winnerVotesMinus[winners[i]]) > 
-                    validation.winnerVotesPlus[winners[i-1]].sub(validation.winnerVotesMinus[winners[i-1]])
-                    )
+                if (plus.sub(minus) > plusBefore.sub(minusBefore))
                     {winner = winners[i];}
         }
-        validation.winner = winner;
         return winner;
     }
 
 
+     function determineWinners(bytes32 validationHash) public view returns (address[] memory, uint256){
+
+        (address[] memory validator, uint256[] memory status, uint256[] memory validationTimes) = insertionSort (validationHash);
+
+        uint256 consensus = determineConsensus(status);
+        bool[] memory isWinner = new bool[](validator.length);
+        bool done;
+        uint256 i=0;
+        uint256 topValidationTime = validationTimes[0];
+        uint256 numFound=0;
+        
+        while (!done) {
+            if (uint256(status[i]) == consensus && validationTimes[i] == topValidationTime){
+                isWinner[i] = true;
+                numFound ++;
+            } 
+         
+            if (i + 1 == validator.length)
+                done = true;
+            else
+                i++;
+          }
+        
+        address[] memory winners = new address[](numFound);
+        uint256 j;
+
+        for (uint256 k = 0; k< validator.length; k++){
+
+            if (isWinner[k]){
+                winners[j] = validator[k];
+                j++;
+            }
+        }
+        return (winners, consensus);
+    }
 
 
+
+ function insertionSort(bytes32 validationHash) internal view returns (address[] memory, uint256[] memory, uint256[] memory) {
+
+        (address[] memory validator, ,uint256[] memory status, uint256[] memory validationTimes,,) =  validations.collectValidationResults(validationHash);
+
+        uint length = validationTimes.length;
+        
+        for (uint i = 1; i < length; i++) {
+            
+            uint key = validationTimes[i];
+            address user = validator[i];
+            uint256 choice = status[i];
+            uint j = i - 1;
+            while ((int(j) > 0) && (validationTimes[j] > key)) {
+                validationTimes[i] = validationTimes[j];
+                validationTimes[i-1] = key; 
+                validator[i] = validator[j];
+                validator[i-1] = user;
+                status[i] = status[j];
+                status[i-1] =  choice; 
+                j--;
+            }
+            validationTimes[j + 1] = key;
+            validator[j+1] = user;
+            status[j+1] = choice;
+        }
+
+        return (validator, status, validationTimes );
+    }
+
+
+
+ function determineConsensus(uint256[] memory validation) public pure returns(uint256 ) {
+
+        uint256 yes;
+        uint256 no;
+
+        for (uint256 i=0; i< validation.length; i++) {
+
+            if (validation[i] == uint256(ValidationStatus.Yes))
+                yes++;
+            else
+                no++;
+        }
+
+        if (yes > no)
+            return 1; // consensus is to approve
+        else if (no > yes)
+            return 2; // consensus is to disapprove
+        else
+            return 2; // consensus is tie - should not happen
+    }
 
 }
