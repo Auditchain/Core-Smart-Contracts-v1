f7a40f4eb7795985b6d6ffce242c899ce86fb1f7
Bogdan Fiedur <win2win@protonmail.com>
2021-10-19 22:58:17 -0500

    add new version of returnWinnerStruct

--- contracts/Auditchain/IValidationHelpers.sol
+++ contracts/Auditchain/IValidationHelpers.sol
@@ -4,7 +4,7 @@ pragma solidity =0.8.0;
 interface IValidatinosHelpers {
 
     function isHashAndTimeCorrect( bytes32 documentHash, uint256 _validationTime) external view returns (bool);
-    function returnWinnerStruct(bytes32 validationHash)external view returns (string memory valUrl, address winner);
+    function returnWinnerStruct(bytes32 validationHash, address contractAddress)external view returns (string memory valUrl, address winner);
     function selectWinner(bytes32 validationHash, address[] memory winners) external view returns (address);
     function determineWinners(bytes32 validationHash) external view returns (address[] memory, uint256);
 
