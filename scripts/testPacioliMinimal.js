6aa807f830f4e073f3aefaaeda6fbebb2783ad4f
Miguel Calejo <mc@interprolog.com>
2021-10-28 14:00:59 +0100

    Add a few more reports, and a wait

--- scripts/testPacioliMinimal.js
+++ scripts/testPacioliMinimal.js
@@ -10,6 +10,7 @@ let BN = require("big-number");
 let HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config({ path: './.env' }); // update process.env
 
+const web3_reader = new Web3(new Web3.providers.WebsocketProvider(process.env.WEBSOCKET_PROVIDER));
 
 const projectId = '1z8qlzYj2AXroPUyrvd4UD70Rd1'
 const projectSecret = '33a8822b1df29fdc33d0930aab075a7b'
@@ -95,6 +96,7 @@ async function setUpContracts(account) {
 var activeSubmissions = 0;
 var submissionErrors = 0;
 var submissionCounter = 0;
+var firstBlock = 0;
 /**
  * 
  * @param {Object} myContractObject 
@@ -132,6 +134,12 @@ function finishedReport(){
     else console.log(`${activeSubmissions} submissions pending`);
 }
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
 async function run() {
     console.log("Setting up accounts...");
     // Setup our report submitters, each with a different account/private key
@@ -139,14 +147,24 @@ async function run() {
     const dataSubscriber2 = await setUpContracts("0x254198af956f996631d6231743b8578f07c9745330081e28af0e29642e896786");
     const dataSubscriber3 = await setUpContracts("0x75e947d1c03407dc87331769842f0c89c915e8a93b480ec055dcafeeca99cac5");
 
+    firstBlock = await web3_reader.eth.getBlockNumber();
+
     const dataSubscribers = [dataSubscriber1,dataSubscriber2,dataSubscriber3];
 
     const reports = [
         "https://xbrlsite.azurewebsites.net/2021/reporting-scheme/proof/reference-implementation/instance.xml",
+        "http://accounting.auditchain.finance/reporting-scheme/proof/reference-implementation/instance_m.xml",
+        "http://accounting.auditchain.finance/reporting-scheme/proof/reference-implementation/instance_f.xml",
+        "http://accounting.auditchain.finance/reporting-scheme/proof/reference-implementation/instance_mf.xml",
+        "http://accounting.auditchain.finance/reporting-scheme/proof/reference-implementation/instance_mft.xml",
+        "http://accounting.auditchain.finance/reporting-scheme/proof/reference-implementation/instance_t.xml",
+        "http://accounting.auditchain.finance/reporting-scheme/proof/reference-implementation/instance_mftdr.xml",
         "https://www.sec.gov/Archives/edgar/data/1318605/000095017021000046/tsla-20210331.htm",
         "https://www.sec.gov/Archives/edgar/data/1108524/000110852417000040/crm-20171031.xml",
         "https://www.sec.gov/Archives/edgar/data/789019/000156459017000654/msft-20161231.xml",
-        "https://www.sec.gov/Archives/edgar/data/1018724/000101872418000005/amzn-20171231.xml"
+        "https://www.sec.gov/Archives/edgar/data/1018724/000101872418000005/amzn-20171231.xml",
+        "http://www.sec.gov/Archives/edgar/data/1600620/000162828018003228/auph-20171231.xml",
+        "http://www.sec.gov/Archives/edgar/data/1651625/000095010318003508/aciu-20171231.xml"
     ];
 
     console.log("Performing submissions...");
@@ -156,9 +174,17 @@ async function run() {
     // else process.exit(1);
 
     // frantic data subscribers:
+    console.log("First lot:")
     for (const DS of dataSubscribers)
         for (const report of reports)
             submitReport(DS,report).then(()=> finishedReport());
+    
+    while( await web3_reader.eth.getBlockNumber()<firstBlock+2 ){
+        console.log("waiting.....");
+        sleep(200);
+    }
+
+    console.log("Second lot:")
     // Again, with warmed caches:
     for (const DS of dataSubscribers)
         for (const report of reports)
