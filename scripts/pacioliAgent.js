bc3173af16702415dec3a48cdad3ee6aa9404d5c
Miguel Calejo <mc@interprolog.com>
2022-01-15 14:25:29 +0000

    Fix buglet in env variable name

--- scripts/pacioliAgent.js
+++ scripts/pacioliAgent.js
@@ -36,7 +36,7 @@ const ropsten_infura_server = process.env.ROPSTEN_INFURA_SERVER;
 const rinkeby_infura_server = process.env.RINKEBY_INFURA_SERVER;
 const main_infura_server = process.env.MAINNET_INFURA_SERVER;
 const goerli_infura_server = process.env.GOERLI_INFURA_SERVER
-const mumbai_server = process.env.MUMBAI_CHAINSTACK_SERVER
+const mumbai_server = process.env.MUMBAI_SERVER
 
 const local_host = process.env.LOCAL;
 const mnemonic = process.env.MNEMONIC;
